d398b1aa7e0c2dd4ecd794612aa9b62a39bbf56c
zak mb <zak26.mb@gmail.com>
2025-09-08 17:27:15 +0100

    add firebase creds

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyAIfen6C-OGnZ77iKeQIlvYzDvdqVvQ668',
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'hockey-fantasy-89cef.firebaseapp.com',
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'hockey-fantasy-89cef',
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'hockey-fantasy-89cef.firebasestorage.app',
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '256587838759',
+	appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:256587838759:web:7ff9643dad24ad9ba7e502'
 }
 
 const app = initializeApp(firebaseConfig)
